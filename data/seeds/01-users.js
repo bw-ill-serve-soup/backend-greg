79d220789c176535a0db79c5f75e9a7416ae2bb2
Zolton <gmzesq@gmail.com>
2019-08-25 18:55:25 -0400

    Getting ready for Heroku deployment

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,11 +1,11 @@
-
+userFunc = require("../../router/userRouterFunctions")
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('users').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {username: "admin", password: "admin"},
+        {username: "admin", password:"asdf"},
         {username: "Rex", password: "asdf"},
         {username: "Susi", password: "asdf"},
         {username: "Calvin", password: "asdf"},
