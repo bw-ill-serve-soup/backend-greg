80f6e2482b5a4aae14d121d07c59a6fd9e0c1854
Zolton <gmzesq@gmail.com>
2019-08-25 20:16:45 -0400

    Eliminated truncate for herko seeds

--- data/seeds/03-weights.js
+++ data/seeds/03-weights.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('weights').truncate()
+  return knex('weights')
     .then(function () {
       // Inserts seed entries
       return knex('weights').insert([
--- data/seeds/04-quantity.js
+++ data/seeds/04-quantity.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('quantity').truncate()
+  return knex('quantity')
     .then(function () {
       // Inserts seed entries
       return knex('quantity').insert([
