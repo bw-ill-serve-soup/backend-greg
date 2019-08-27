4a920d51f91b982fd1260ec869f8366a132c22b5
Zolton <gmzesq@gmail.com>
2019-08-26 23:34:28 -0400

    Added .catch where missing

--- kitchenRouter/kitchenRouter.js
+++ kitchenRouter/kitchenRouter.js
@@ -16,8 +16,13 @@ router.get("/", (req, res) => {
 
 router.get("/inventory", (req, res) => {
   let id = req.userInfo.subject;
-  kitchenHelper.getUserInventory(id).then(userInventory => {
+  kitchenHelper
+  .getUserInventory(id)
+  .then(userInventory => {
     res.status(200).json(userInventory);
+  })
+  .catch(error => {
+    res.status(500).json({ Error: "Something's gone horribly wrong" });
   });
 });
 
@@ -87,6 +92,9 @@ router.delete("/inventory", kitchenHelper.addUserID, (req, res) => {
           res.status(200).json(delItem);
         });
       }
+    })
+    .catch(error => {
+      res.status(500).json({ Error: "Something's gone horribly wrong" });
     });
 });
 
